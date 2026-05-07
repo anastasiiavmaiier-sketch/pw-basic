5e07f367c683d364e1e4844bfb8df9e65d0b4707
anastasiiavmaiier-sketch <anastasiia.v.maiier@gmail.com>
2026-05-07 17:38:52 +0300

    Update eslint.config.mjs
    Co-authored-by: Nataliia Kubytska <94222380+natakub@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,8 @@ export default tseslint.config(
   },
 {
     rules: {
-    '@typescript-eslint/no-explicit-any': 'error'
+    '@typescript-eslint/no-explicit-any': 'error',
+    "@typescript-eslint/no-floating-promises": "error"
     }
 },
     {
